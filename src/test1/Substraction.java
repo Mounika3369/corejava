c62a08e883f3078590f55939bb73769107b375b4
mounika <mounika3369@gmail.com>
2021-03-03 15:14:36 +0530

    Third commit

---
+++ src/test1/Substraction.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Substraction {
+	public void m1(int a, int b) {
+		System.out.println(a - b);
+	}
+
+	public static void main(String[] args) {
+		Substraction obj = new Substraction();
+		obj.m1(20, 10);
+
+	}
+
+}
